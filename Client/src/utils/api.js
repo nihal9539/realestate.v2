4e272a5c02da3bf3003c08f0c9960099c00cdfd7
Mohammed NIhal v K <mohammednihal691@gmail.com>
2023-12-17 16:43:52 +0530

    update

--- Client/src/utils/api.js
+++ Client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs"
 import { toast } from "react-toastify"
 
 export const api = axios.create({
-    baseURL: "https://realestate-beta-nine.vercel.app//api"
+    baseURL: "http://localhost:8000/api"
 })
 
 
